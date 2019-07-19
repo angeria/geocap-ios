13ec45350582513b9de34bc036fd8bb59cbccb2d
Benjamin Angeria <benjamin@angeria.se>
2019-07-19 14:44:01 +0200

    Initial Commit

---
+++ GeoCap/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GeoCap
+//
+//  Created by Benjamin Angeria on 2019-07-19.
+//  Copyright © 2019 Benjamin Angeria. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
