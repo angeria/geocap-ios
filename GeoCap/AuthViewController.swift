29f57cbd747acbcbdddd7a1118abeec462079d31
Benjamin Angeria <benjamin@angeria.se>
2019-08-04 08:58:19 +0200

    Improve sign in module

--- GeoCap/AuthViewController.swift
+++ GeoCap/AuthViewController.swift
@@ -11,36 +11,38 @@ import Firebase
 import FirebaseUI
 
 class AuthViewController: UIViewController, FUIAuthDelegate {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
     var authListener: AuthStateDidChangeListenerHandle?
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        let authViewController = self.authUI.authViewController()
+        self.present(authViewController, animated: true)
+        
+        // The closure might fire twice during initialization if an user is logged in
+        // First time with user as nil and second time with a user
+        // Thus the need for authListenerIsInitalized to prevent incorrect segueing
         authListener = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
-            if let _ = user {
-                self?.performSegue(withIdentifier: "Show Map", sender: nil)
-            } else {
-                if let authViewController = self?.authUI.authViewController() {
-                    self!.present(authViewController, animated: true)
-                }
+            guard let self = self else { return }
+            
+            print("fired")
+            
+            if user != nil {
+                self.performSegue(withIdentifier: "Show Map", sender: nil)
             }
         }
     }
-
+    
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         
         if authListener != nil {
             Auth.auth().removeStateDidChangeListener(authListener!)
         }
+        
+        try? authUI.signOut()
     }
-    
-    // MARK: - Authorization
     
     lazy var authUI: FUIAuth = {
         let authUI = FUIAuth.defaultAuthUI()!
@@ -53,7 +55,7 @@ class AuthViewController: UIViewController, FUIAuthDelegate {
         switch error {
         case .none:
             if let user = authDataResult?.user {
-                saveUser(user)
+                storeUser(user)
             }
         case .some(let error):
             handleSignInError(error)
@@ -64,10 +66,9 @@ class AuthViewController: UIViewController, FUIAuthDelegate {
         let error = error as NSError
         let errorCode = FUIAuthErrorCode(rawValue: UInt(error.code))
         
-        // TODO: Handle errors
         switch errorCode {
         case .some where errorCode == .userCancelledSignIn:
-            print("User cancelled sign-in")
+            break
         case .some where errorCode == .providerError:
             print("Login error from provider: \(error.userInfo[FUIAuthErrorUserInfoProviderIDKey]!)")
             fallthrough
@@ -76,10 +77,20 @@ class AuthViewController: UIViewController, FUIAuthDelegate {
             if error.userInfo[NSUnderlyingErrorKey] != nil {
                 print("Underlying login error: \(error.userInfo[NSUnderlyingErrorKey]!)")
             }
+            presentLoginErrorAlert()
         }
     }
     
-    private func saveUser(_ user: User) {
+    private func presentLoginErrorAlert() {
+        let title = "Sign In Failed"
+        let message = "Something went wrong when signing in, please try again"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    private func storeUser(_ user: User) {
         let db = Firestore.firestore()
         db.collection("users").document(user.uid).setData([
             // TODO: Only unique names should be allowed. Maybe remove default to empty string?
@@ -94,11 +105,4 @@ class AuthViewController: UIViewController, FUIAuthDelegate {
         }
     }
     
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-
 }
--- GeoCap/MapViewController.swift
+++ GeoCap/MapViewController.swift
@@ -21,10 +21,12 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
 
     private lazy var db = Firestore.firestore()
     
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapView.register(LocationAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(Location.self))
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(Location.self))
         
         fetchLocations()
     }
@@ -61,6 +63,19 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         }
     }
     
+    // MARK: - Annotations
+    
+    private func fetchLocations() {
+        db.collection("cities").document("uppsala").collection("locations").getDocuments { [weak self] (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                let locations = querySnapshot!.documents.compactMap { Location(data: $0.data()) }
+                self?.mapView.addAnnotations(locations)
+            }
+        }
+    }
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard !annotation.isKind(of: MKUserLocation.self) else { return nil }
         
@@ -74,26 +89,24 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     private func setupLocationAnnotationView(for annotation: Location, on mapView: MKMapView) -> MKAnnotationView {
         let reuseIdentifier = NSStringFromClass(Location.self)
-        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier, for: annotation)
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier, for: annotation) as! MKMarkerAnnotationView
+        
+        annotationView.animatesWhenAdded = true
         annotationView.canShowCallout = true
+        
+        let captureButton = UIButton(type: .system)
+        captureButton.setTitle("Capture", for: .normal)
+        captureButton.tintColor = .white
+        captureButton.backgroundColor = UIColor.Custom.systemBlue
+        // TODO: Extract constants and adjust to different text sizes
+        captureButton.frame = CGRect(x: 0, y: 0, width: 90, height: 50)
+        annotationView.rightCalloutAccessoryView = captureButton
+        
         return annotationView
     }
     
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
         performSegue(withIdentifier: "Show Quiz", sender: nil)
-    }
-    
-    // MARK: - Locations
-    
-    private func fetchLocations() {
-        db.collection("cities").document("uppsala").collection("locations").getDocuments { [weak self] (querySnapshot, error) in
-            if let error = error {
-                print("Error getting documents: \(error)")
-            } else {
-                let locations = querySnapshot!.documents.compactMap { Location(data: $0.data()) }
-                self?.mapView.addAnnotations(locations)
-            }
-        }
     }
     
     // MARK: - User Location Authorization
@@ -102,12 +115,12 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     private func requestUserLocationAuth() {
         switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse, .authorizedAlways:
+            break
         case .notDetermined:
             locationManager.requestWhenInUseAuthorization()
         case .denied, .restricted:
             presentLocationAccessDeniedAlert()
-        case .authorizedWhenInUse, .authorizedAlways:
-            break
         @unknown default:
             break
         }
@@ -115,10 +128,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     // TODO: String localization
     private func presentLocationAccessDeniedAlert() {
-        let title = "Location Access Denied"
-        let message = "Access to your location was denied, please enable location services to be able to play."
+        let title = "Location Services Off"
+        let message = "Turn on location services in settings to allow GeoCap to determine your current location"
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default)
+        let settingsAction = UIAlertAction(title: "Settings", style: .default, handler: {action in
+            UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
+        })
+        alert.addAction(settingsAction)
         alert.addAction(okAction)
         present(alert, animated: true)
     }
