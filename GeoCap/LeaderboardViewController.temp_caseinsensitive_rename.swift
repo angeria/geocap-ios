aeec39cbb8915990a9bbb724cc28d07acfdaf883
Benjamin Angeria <benjamin@angeria.se>
2019-08-09 16:01:59 +0200

    Add boilerplate for leaderboard

---
+++ GeoCap/LeaderboardViewController.temp_caseinsensitive_rename.swift
@@ -0,0 +1,121 @@
+//
+//  leaderboardViewController.swift
+//  GeoCap
+//
+//  Created by Benjamin Angeria on 2019-08-09.
+//  Copyright © 2019 Benjamin Angeria. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class leaderboardViewController: UITableViewController {
+
+    private var userListener: ListenerRegistration?
+    private lazy var db = Firestore.firestore()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fetchUsers()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    
+    private func fetchUsers() {
+        userListener = db.collection("users").addSnapshotListener { [weak self] querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                print("Error fetching users: \(error!)")
+                return
+            }
+            
+            snapshot.documentChanges.forEach { diff in
+                guard let self = self else { return }
+                
+                if (diff.type == .added) {
+
+                }
+                
+                if (diff.type == .modified) {
+                    
+                }
+                
+                if (diff.type == .removed) {
+                    
+                }
+            }
+        }
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
