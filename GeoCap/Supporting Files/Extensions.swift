f898eb763417c456f2ca18c7701f2ea2ddf24f13
Benjamin Angeria <benjamin@angeria.se>
2019-09-10 11:09:23 +0200

    Close quiz when time runs out

--- GeoCap/Supporting Files/Extensions.swift
+++ GeoCap/Supporting Files/Extensions.swift
@@ -38,7 +38,7 @@ extension UIView {
     func shake() {
         let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
-        animation.duration = 0.4
+        animation.duration = GeoCap.Constants.shakeAnimationDuration
         animation.values = [-7.5, 7.5, -5.0, 5.0, -2.5, 2.5, 0.0]
         self.layer.add(animation, forKey: "shake")
     }
