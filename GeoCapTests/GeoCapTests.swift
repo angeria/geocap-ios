73eafdb31c57fabc6837dfdce98f3b00cd318b1e
Benjamin Angeria <benjamin@angeria.se>
2019-12-01 14:45:22 +0100

    Update deps

--- GeoCapTests/GeoCapTests.swift
+++ GeoCapTests/GeoCapTests.swift
@@ -3,11 +3,12 @@
 //  GeoCapTests
 //
 //  Created by Benjamin Angeria on 2019-07-19.
-//  Copyright © 2019 Benjamin Angeria. All rights reserved.
+//  Copyright © 2019 Benjamin Angeria. All righ wts reserved.
 //
 
 import XCTest
 @testable import GeoCap
+@testable import Firebase
 
 class GeoCapTests: XCTestCase {
 
@@ -19,16 +20,21 @@ class GeoCapTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testCreateLocation() {
+        let location = Location(data: [
+            "name": "Test location",
+            "center": GeoPoint(latitude: 32.4334, longitude: 23.4334)
+        ], username: "John")
+        
+        XCTAssert(location != nil)
+        XCTAssertFalse(location!.isCapturedByUser)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//        // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
